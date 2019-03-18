f4ea54bbf654caab98807bb3c1c7ec5bca3db096
Sarah Thian <sthian@moneylion.com>
2019-03-18 22:22:04 +0800

    import ReadableType

--- android/src/main/java/com/launchdarkly/RNLaunchDarklyModule.java
+++ android/src/main/java/com/launchdarkly/RNLaunchDarklyModule.java
@@ -14,6 +14,7 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.common.collect.Sets;
 import com.launchdarkly.android.FeatureFlagChangeListener;
